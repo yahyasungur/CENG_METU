8868b4e5d4638010201b467855288a56266e963c
Yahya SUNGUR <yhysngr18@gmail.com>
2022-03-11 19:10:30 +0300

    Ceng213 files added.

---
+++ Ceng213/THE1/main_string.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+#include "String.h"
+#include "LinkedList.h"
+
+int main() {
+/*     std::cout << "-> Creating a string with string constructor." << std::endl;
+
+    char i[] = "hello world";
+    String str1(i);
+
+    std::cout << "-> spliting " "" << std::endl;
+
+
+    str1.split('l').traverse();
+    str1.print(true);
+
+    str1.eraseCharacters(0,1);
+    
+    str1.print(true);
+
+    str1.eraseCharacters(3,1);
+    
+    str1.print(true);
+
+    str1.eraseCharacters(2,1);
+    
+    str1.print(true);
+
+    str1.eraseAllCharacters();
+    
+    str1.print(true);
+
+    std::cout << str1.isEmpty() << std::endl;
+
+    str1.insertCharacter('w',0);
+    str1.appendCharacter('d');
+    str1.insertCharacter('l',-1);
+    str1.insertCharacter('r',1);
+    str1.insertCharacter('w',0);
+
+    str1.print(true);
+
+    std::cout << "-> Creating a string with default constructor." << std::endl;
+
+    char chr[] = "data structures";
+    String str2(chr);
+
+    std::cout << "-> Printing the string." << std::endl;
+
+    str2.print(true); */
+
+    return 0;
+}
